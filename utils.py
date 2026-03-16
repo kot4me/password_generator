656e7a0ebf286cd9c96a53fb460dca1764fba2e2
Dmitrii <kislyakovdimawork@gmail.com>
2026-03-16 10:58:56 +0300

    Добавлена улучшенная функция complex_password (Гармаев) в utils.py

--- utils.py
+++ utils.py
@@ -27,3 +27,23 @@ def simple_password(length=8):
     simple_charset = config.LOWERCASE + config.UPPERCASE + config.DIGITS
     password = ''.join(random.choice(simple_charset) for _ in range(length))
     return password
+
+def complex_password(length=config.RECOMMENDED_COMPLEX_LENGTH):
+    complex_charset = (config.LOWERCASE + config.UPPERCASE + 
+                       config.DIGITS + config.SPECIAL)
+    
+    if config.REQUIRE_ALL_CHAR_TYPES:
+        password = [
+            random.choice(config.LOWERCASE),
+            random.choice(config.UPPERCASE),
+            random.choice(config.DIGITS),
+            random.choice(config.SPECIAL)
+        ]
+        
+        for _ in range(length - 4):
+            password.append(random.choice(complex_charset))
+        
+        random.shuffle(password)
+        return ''.join(password)
+    else:
+        return ''.join(random.choice(complex_charset) for _ in range(length))
